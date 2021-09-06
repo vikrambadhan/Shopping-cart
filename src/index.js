aeac96dcfe24d446cb725e02adc34ecdf6bbb386
vikrambadhan <vikram.badhan@gmail.com>
2021-09-06 15:38:01 -0400

    Setting up firebase and adding it to shopping-cart

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBg5fBQCqkQdXUE87yNJoKwEIjbjaKd3Qo",
+    authDomain: "shopping-cart-5e59b.firebaseapp.com",
+    projectId: "shopping-cart-5e59b",
+    storageBucket: "shopping-cart-5e59b.appspot.com",
+    messagingSenderId: "943917775122",
+    appId: "1:943917775122:web:3ee5c65c68a53408b21bcb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
@@ -10,5 +24,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
